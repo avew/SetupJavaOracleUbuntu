a2f538733e071e65a78e95fa27a1557b1ae8f1b1
Asep Rojali <aseprojali@gmail.com>
2013-06-14 21:12:27 +0700

    Update setup-java-oracle-ubuntu.sh

--- setup-java-oracle-ubuntu.sh
+++ setup-java-oracle-ubuntu.sh
@@ -17,7 +17,6 @@ sudo tar zxvf jdk-7u21-linux-i586.tar.gz
 sudo tar zxvf jre-7u21-linux-i586.tar.gz
 sudo rm jdk-7u21-linux-i586.tar.gz
 sudo rm jre-7u21-linux-i586.tar.gz
-ls -l
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_21/bin/javac" 1
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jre1.7.0_21/bin/javac" 1
 sudo update-alternatives --set "javac" "/usr/lib/jvm/jdk1.7.0_21/bin/javac"
@@ -27,5 +26,3 @@ sudo echo "PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
 sudo echo "export JAVA_HOME" >> /etc/profile
 sudo echo "export PATH" >> /etc/profile
 . /etc/profile
-java –version
-javac –version
